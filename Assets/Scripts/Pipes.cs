7dbc0fd3c27ed2b565512509de74454f5f23fd67
Lucas <lucascruzestudo@gmail.com>
2024-04-10 21:29:08 -0300

    Segundo commit

---
+++ Assets/Scripts/Pipes.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private void Update()
+    {
+        transform.position += speed
+            * Time.deltaTime * Vector3.left;
+    }
+}
